d6d66507b02849f67281300a72a88e668b7543fd
Jemo69 <jemolife69@gmail.com>
2025-11-17 21:01:19 -0500

    FORMATTED : file next config

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '18nlg60l9z.ufs.sh',
-                port: '',
-                pathname: '/f/**',
-            },
-            {
-                protocol: 'https',
-                hostname: '8331whtezt.ufs.sh',
-                port: '',
-                pathname: '/f/**',
-            },
-        ],
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "18nlg60l9z.ufs.sh",
+        port: "",
+        pathname: "/f/**",
+      },
+      {
+        protocol: "https",
+        hostname: "8331whtezt.ufs.sh",
+        port: "",
+        pathname: "/f/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
